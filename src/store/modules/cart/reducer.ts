12916fc76982b8d80e0130e930e1ea7adf3c3e9f
Arimario Jesus <arimario.jesus@hotmail.com>
2021-12-09 21:29:32 -0300

    fix(cart/reducer): prevent add same product, instead add to the current amount

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -16,11 +16,19 @@ const cart: Reducer<ICartState, AnyAction> = (
       case "ADD_PRODUCT_TO_CART":
         const { product } = action.payload;
 
-        draft.items.push({
-          product,
-          quantity: 1,
-        });
-        
+        const productInCartIndex = draft.items.findIndex(
+          (item) => item.product.id === product.id
+        );
+
+        if (productInCartIndex >= 0) {
+          draft.items[productInCartIndex].quantity++;
+        } else {
+          draft.items.push({
+            product,
+            quantity: 1,
+          });
+        }
+
         break;
       default:
         return draft;
